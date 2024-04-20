1af01228c233f42b69103053103ad30e85ff3230
João Vitor Bernardis <joaobernardiss@gmail.com>
2024-04-20 17:55:14 -0300

    Finished flutter gas_station_application

--- DDM/04.05/gas_station_application/lib/gas.dart
+++ DDM/04.05/gas_station_application/lib/gas.dart
@@ -12,13 +12,17 @@ class GasStation extends StatefulWidget {
 class _GasStationState extends State<GasStation> {
 
   TextEditingController controllerAlcohol = TextEditingController();
+  TextEditingController controllerGasoline = TextEditingController();
+
+  Text finalResult = Text("");
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Álcool ou Gasolina'),
+        title: Text('Álcool x Gasolina', style: TextStyle(color: Colors.white),),
         centerTitle: true,
+        backgroundColor: Colors.red.shade800,
       ),
       body: Container(
         padding: EdgeInsets.all(30),
@@ -26,13 +30,79 @@ class _GasStationState extends State<GasStation> {
           child: Column(
             children: [
               Image.asset('assets/images/texaco.png', width: 150,),
+
+              SizedBox(height: 65,),
+
               TextField(
               controller: controllerAlcohol,
-
                 decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.local_gas_station_outlined),
+                  label: Text("Preço do Álcool por L"),
                   border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
                 ),
               ),
+
+              SizedBox(height: 15,),
+
+              TextField(
+              controller: controllerGasoline,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.local_gas_station_outlined),
+                  label: Text("Preço da Gasolina por L"),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+
+              SizedBox(height: 15,),
+
+              ElevatedButton(
+                child: Text("Comparar", style: TextStyle(fontSize: 25, color: Colors.white),),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(18),
+                  backgroundColor: Colors.red.shade800,
+                ),
+
+                onPressed: () {
+                  double alcohol = double.tryParse(controllerAlcohol.text)!;
+                  double gasoline = double.tryParse(controllerGasoline.text)!;
+
+
+                  if(controllerAlcohol.text == "" || controllerGasoline.text == "") {
+                    finalResult = Text(
+                      "Não podem haver campos vazios!",
+                      style: TextStyle(
+                        color: Colors.red.shade900,
+                        fontSize: 30,
+                      ),
+                    );
+                  }
+                  else if(alcohol / gasoline >= 0.7) {
+                    finalResult = Text(
+                      "Abasteça com gasolina!",
+                      style: TextStyle(
+                        color: Colors.red.shade900,
+                        fontSize: 30,
+                      ),
+                    );
+                  }
+                  else {
+                    finalResult = Text(
+                      "Abasteça com álcool!",
+                      style: TextStyle(
+                        color: Colors.red.shade900,
+                        fontSize: 30,
+                      ),
+                    );
+                  }
+
+                  setState(() {});
+                },
+              ),
+
+              SizedBox(height: 25,),
+
+              finalResult,
+
             ],
           ),
         )
