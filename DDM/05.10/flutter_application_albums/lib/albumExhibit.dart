cc50e822d6aeaad4b90ab4ae9ae2e219aa60ae5d
Enrico Lima <210408enrico@gmail.com>
2024-05-24 09:33:31 -0300

    terminei o flutter gracas a deus

--- DDM/05.10/flutter_application_albums/lib/albumExhibit.dart
+++ DDM/05.10/flutter_application_albums/lib/albumExhibit.dart
@@ -9,32 +9,98 @@ import 'package:flutter_application_albums/track.dart';
 
 class AlbumExhibit extends StatefulWidget {
   Album album = Album("", "", [], "");
-  AlbumExhibit(this.album, {super.key});
+  bool whiteText = false;
+  AlbumExhibit(this.album, this.whiteText, {super.key});
 
   @override
   State<AlbumExhibit> createState() => _AlbumExhibitState();
 }
 
 class _AlbumExhibitState extends State<AlbumExhibit> {
+  
+
+  
+  Row printTrack(Track track) {
+    return Row(
+      
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+      children: [
+        Text(track.title),
+        Text(track.length)
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    Color textColor = Colors.black;
+
+    if(widget.whiteText == true) {
+      textColor = Colors.white;
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.album.title),
-        backgroundColor: Colors.transparent,
-      ),
+      backgroundColor: Colors.transparent,
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: ExactAssetImage(widget.album.albumCoverPath),
+            image: AssetImage(widget.album.albumCoverPath),
             fit: BoxFit.cover,
-          ),
+          )
         ),
         child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-          child: Stack(),
+          filter: ImageFilter.blur(sigmaX: 7.0, sigmaY: 7.0),
+          child: Center(
+            child: Column(
+              children: [
+                AppBar(
+                  title: Text(widget.album.title, style: TextStyle(color: textColor),),
+                  backgroundColor: Colors.transparent,
+                ),
+
+                Text(widget.album.title, style: TextStyle( fontSize: 40, color: textColor,), textAlign: TextAlign.center, ),
+                Text("by ${widget.album.author}", style: TextStyle( fontSize: 24, color: textColor), textAlign: TextAlign.center,),
+
+                Padding(
+                  
+                  padding: EdgeInsets.all(10),
+                  child: Container(
+
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+
+                    padding: EdgeInsets.all(10),
+
+                    child: ListView.builder(
+                    
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                    
+                      itemCount: widget.album.tracklist.length,
+                      itemBuilder: (context, index) {
+                          
+                        return Row(
+                        
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        
+                          children: [
+                            Text(widget.album.tracklist[index].title, style: TextStyle(fontSize: 16),),
+                            Text(widget.album.tracklist[index].length, style: TextStyle(fontSize: 16),),
+                          ],
+                        );
+                      }
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
-      ),
+      )
     );
   }
 }
